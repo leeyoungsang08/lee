d48abd0c79084e8e674ec7303ff9ef6586d86f2f
leeyoungsang <korea7952@gmail.com>
2026-09-02 10:23:39 +0900

    day2.c 중간 작성

---
+++ day2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hi\n");
+    printf("my name is\tlee young sang\n");
+
+    printf("i like soccec\br\n");
+    printf("i like soccer\rU\n");
+
+    printf("%d\n", 10);
+    printf("%lf\n", 0.12345);
+    printf("%.1lf\n", 0.16345);
+    printf("%.2lf\n", 0.12345);
+
+    printf("%d와 %d의 합은 %d입니다.",1,2,1+2);
+
+    
+
+    return 0;
+}
